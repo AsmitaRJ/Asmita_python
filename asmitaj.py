3edee1c40b57e77feca0d8cfd1dea70a16343299
AsmitaRJ <147146937+AsmitaRJ@users.noreply.github.com>
2023-11-29 14:20:06 +0530

    Add files via upload

--- asmitaj.py
+++ asmitaj.py
@@ -1,10 +1,10 @@
 import mysql.connector as MyConn
-#  print("Driver started!")
+# print("Driver started!")
 
-mydb=MyConn.connect(host="localhost",username="root",password=" ")
-print(mydb,"connection established")
+mydb=MyConn.connect(host="localhost",username="root",password="root123",database="nikkii")
+# print(mydb,"connection established")
 
-# db_cursor=mydb.cursor()
+db_cursor=mydb.cursor()
 
 # db_cursor.execute('create database nikkii')
 # print("database created")
@@ -12,6 +12,7 @@ print(mydb,"connection established")
 # db_cursor.execute('create table emp(Number int, Name varchar(30), Salary int)')
 # print("table created!")
 
+# inserting the data
 # db_cursor.execute('INSERT INTO emp VALUES (1, "Asmita", 100)')
 # db_cursor.execute('INSERT INTO emp VALUES (2, "shreeya", 200)')
 # db_cursor.execute('INSERT INTO emp VALUES (3, "Sarvesh", 300)')
@@ -30,7 +31,8 @@ print(mydb,"connection established")
 # mydb.commit()
 # print("Updated!")
 
-# db_deletedata= "delete from Emp where Number=%s"
-# db_value=(2,)
-# db_cursor.execute(db_deletedata,db_value)
-# print("Deleted")
+db_deletedata= "delete from emp where Number=%s"
+db_value=(2,)
+db_cursor.execute(db_deletedata,db_value)
+mydb.commit()
+print("Deleted")
